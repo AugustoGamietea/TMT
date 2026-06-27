ac83ff63bda312a26a56b3631c1abf1942724773
AE GJ <gamieteaaugusto@gmail.com>
2026-06-27 16:03:17 -0300

    Locales

--- componentes/Locales.jsx
+++ componentes/Locales.jsx
@@ -10,72 +10,72 @@ import Home from './Home'
 export default function Locales() {
     return (
         <>
-        <Home />
-        <div className='cont-locales'>
-            <div className="locales">
-                <h2>Conoce nuestros locales</h2>
-                <div className="local">
-                    <img src={local} className="local-img" />
-                    <div className="local-info">
-                        <h3>Local 1</h3>
+            <Home />
+            <div className='cont-locales'>
+                <div className="locales">
+                    <h2>Conoce nuestros locales</h2>
+                    <div className="local">
+                        <img src={local} className="local-img" />
+                        <div className="local-info">
+                            <h3>Local 1</h3>
 
-                        <div className="calle">
-                            <img src={calle} />
-                            <p>Alem</p>
+                            <div className="calle">
+                                <img src={calle} />
+                                <p>Alem</p>
+                            </div>
+                            <div className="provincia">
+                                <img src={provincia} />
+                                <p>Tierra del Fuego</p>
+                            </div>
+                            <div className="telefono">
+                                <img src={telefono} />
+                                <p>2901 234 567</p>
+                            </div>
                         </div>
-                        <div className="provincia">
-                            <img src={provincia} />
-                            <p>Tierra del Fuego</p>
-                        </div>
-                        <div className="telefono">
-                            <img src={telefono} />
-                            <p>2901 234 567</p>
+                        <div className="separador"></div>
+                        <div className="extra">
+                            <div className="maps">
+                                <img src={maps} />
+                                <p>Ver maps</p>
+                            </div>
+                            <div className="mail">
+                                <img src={mail} />
+                                <p>Enviar mail</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="separador"></div>
-                    <div className="extra">
-                        <div className="maps">
-                            <img src={maps} />
-                            <p>Ver maps</p>
-                        </div>
-                        <div className="mail">
-                            <img src={mail} />
-                            <p>Enviar mail</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="local">
-                    <img src={local} className="local-img" />
-                    <div className="local-info">
-                        <h3>Local 2</h3>
+                    <div className="local">
+                        <img src={local} className="local-img" />
+                        <div className="local-info">
+                            <h3>Local 2</h3>
 
-                        <div className="calle">
-                            <img src={calle} />
-                            <p>Alem</p>
+                            <div className="calle">
+                                <img src={calle} />
+                                <p>Alem</p>
+                            </div>
+                            <div className="provincia">
+                                <img src={provincia} />
+                                <p>Tierra del Fuego</p>
+                            </div>
+                            <div className="telefono">
+                                <img src={telefono} />
+                                <p>2901 234 567</p>
+                            </div>
                         </div>
-                        <div className="provincia">
-                            <img src={provincia} />
-                            <p>Tierra del Fuego</p>
-                        </div>
-                        <div className="telefono">
-                            <img src={telefono} />
-                            <p>2901 234 567</p>
-                        </div>
-                    </div>
-                    <div className="separador"></div>
-                    <div className="extra">
-                        <div className="maps">
-                            <img src={maps} />
-                            <p>Ver maps</p>
-                        </div>
-                        <div className="mail">
-                            <img src={mail} />
-                            <p>Enviar mail</p>
+                        <div className="separador"></div>
+                        <div className="extra">
+                            <div className="maps">
+                                <img src={maps} />
+                                <p>Ver maps</p>
+                            </div>
+                            <div className="mail">
+                                <img src={mail} />
+                                <p>Enviar mail</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
