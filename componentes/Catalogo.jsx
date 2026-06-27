742f7a14307db9115866a9d795bf18e3a490ff8c
Mateo-LR <mateolewelruiz200@gmail.com>
2026-06-27 00:33:16 -0300

    locales

--- componentes/Catalogo.jsx
+++
@@ -1,5 +0,0 @@
-export default function Catalogo() {
-    return (
-        <div className="vacio">Asignado a Danae</div>
-    )
-}
--- componentes/Terminos_y_condiciones.jsx
+++
@@ -1,5 +0,0 @@
-export default function Terminos_y_condiciones() {
-    return (
-        <div className="vacio">Asignado a Danae</div>
-    )
-}
