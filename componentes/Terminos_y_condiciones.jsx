efd28d165d9915b20c1420891c3ebec4b016e722
AE GJ <gamieteaaugusto@gmail.com>
2026-06-27 23:25:51 -0300

    Terminos y condiciones

---
+++ componentes/Terminos_y_condiciones.jsx
@@ -0,0 +1,44 @@
+import "../styles/Terminos_y_condiciones.css"
+import Escudo from "../assets/escudo.svg"
+import Documento from "../assets/papel.svg"
+
+export default function Terminos_y_condiciones() {
+    return (
+        <div className="terminos">
+            <h1>Terminos y condiciones</h1>
+
+            <div className="cards-info">
+                <div className="CardInfo">
+                    <div className="card-icon">
+                        <img src={Escudo} alt='Términos y condiciones' />
+                    </div>
+                    <h2>Términos y condiciones</h2>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                    </ul>
+                    <button className="btn">Leer más</button>
+                </div>
+                <div className="CardInfo">
+                    <div className="card-icon">
+                        <img src={Documento} alt='Politicas de privacidad' />
+                    </div>
+                    <h2>Politicas de privacidad</h2>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </li>
+                    </ul>
+                    <button className="btn">Leer más</button>
+                </div>
+            </div>
+        </div>
+    )
+}
