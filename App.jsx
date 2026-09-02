4156d4b43defe5efea16802418bdf810085a0315
AE GJ <gamieteaaugusto@gmail.com>
2026-09-02 20:26:11 -0300

    array productos

--- App.jsx
+++ App.jsx
@@ -12,6 +12,20 @@ import Terminos_y_condiciones from './componentes/Terminos_y_condiciones'
 import Preguntas_frecuentes from './componentes/Preguntas_frecuentes'
 
 export default function App() {
+    const productos = [
+        {
+            nombre: 'Silla',
+            categoria: 'Living',
+            precio: 1000,
+            colores: [
+                '3ad78fc', '7987c9', 'fa2261'
+            ],
+            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            material: 'Nogal',
+            imagenes: []
+        }
+    ];
+
     return (
         <Router >
             <Header />
