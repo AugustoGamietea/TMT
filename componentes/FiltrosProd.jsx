e6c4f03aa591a0ac407754e895b3b9380e44442b
unknown <olivagonzalogerman@gamil.com>
2026-09-02 21:12:01 -0300

    json filtros

--- componentes/FiltrosProd.jsx
+++ componentes/FiltrosProd.jsx
@@ -1,72 +1,28 @@
+import datos from "../datos.json"
+
 export default function FiltrosProd() {
+
+    console.log(datos);
     return (
         <div className="filtros">
             <div className="grid-filtro">
                 <h2>Categorias</h2>
-                <label>
-                    <input type="radio" name="categoria" />
-                    <span>Cocina</span>
-                </label>
-                <label>
-                    <input type="radio" name="categoria" />
-                    <span>Comedor</span>
-                </label>
-                <label>
-                    <input type="radio" name="categoria" />
-                    <span>Living</span>
-                </label>
-                <label>
-                    <input type="radio" name="categoria" />
-                    <span>Oficina</span>
-                </label>
-                <label>
-                    <input type="radio" name="categoria" />
-                    <span>Dormitorio</span>
-                </label>
-                <label>
-                    <input type="radio" name="categoria" />
-                    <span>Cajones</span>
-                </label>
+                {datos.categoria.map((categoria) => (
+                    <label key={categoria}>
+                        <input type="radio" name="categoria" />
+                        <span className="filtro-span">{categoria}</span>
+                    </label>
+                ))}
             </div>
 
             <div className="grid-filtro">
                 <h2>Madera</h2>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Pino</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Abedul</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Roble</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Cedro</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Eucalipto</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Nogal</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Cerezo</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Algarobbo</span>
-                </label>
-                <label>
-                    <input type="radio" name="madera" />
-                    <span>Fresno</span>
-                </label>
+                 {datos.madera.map((madera) => (
+                    <label key={madera}>
+                        <input type="radio" name="madera" />
+                        <span className="filtro-span">{madera}</span>
+                    </label>
+                ))}
             </div>
         </div>
     )
