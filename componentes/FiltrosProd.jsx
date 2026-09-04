20c2469cf3f88c5a47136f826031da579be34ed7
unknown <olivagonzalogerman@gamil.com>
2026-09-04 20:45:04 -0300

    .map productos + filtrado

--- componentes/FiltrosProd.jsx
+++ componentes/FiltrosProd.jsx
@@ -1,15 +1,20 @@
 import datos from "../datos.json"
+import { useLocation } from "wouter";
 
 export default function FiltrosProd() {
+    const [location, setLocation] = useLocation();
 
-    console.log(datos);
+    const filtro = location.split("/");
+
+    console.log(filtro[filtro.length - 1]);
+    
     return (
         <div className="filtros">
             <div className="grid-filtro">
                 <h2>Categorias</h2>
                 {datos.categoria.map((categoria) => (
                     <label key={categoria}>
-                        <input type="radio" name="categoria" />
+                        <input type="radio" name="categoria" checked={filtro[filtro.length - 1] == categoria} onChange={() => setLocation(`/Catalogo/${categoria}`)} />
                         <span className="filtro-span">{categoria}</span>
                     </label>
                 ))}
--- componentes/ListadoProd.jsx
+++ componentes/ListadoProd.jsx
@@ -1,26 +1,18 @@
 import Tarjeta from "./Tarjeta"
+import datos from "../datos.json"
+import { useLocation } from "wouter";
 
 export default function ListadoProd () {
+    const [location, ] = useLocation();
+
+    const filtro = location.split("/");
+    const filtroSelec = filtro[filtro.length - 1];
+    
     return (
         <div className="Listado">
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
-            <Tarjeta></Tarjeta>
+            {datos.productos.filter(p => {if (filtroSelec == p.categoria || filtroSelec == "Catalogo") {return true}}).map((producto) => (
+                <Tarjeta key={producto.categoria} producto={producto}></Tarjeta>
+            ))}
         </div>
     )
 }
