667fc35e6b0460c793f45fd96e7b53a05d461f4c
unknown <olivagonzalogerman@gamil.com>
2026-09-09 14:35:36 -0300

    actualizacion de filtrado de productos

--- componentes/ListadoProd.jsx
+++ componentes/ListadoProd.jsx
@@ -5,31 +5,32 @@ import { useLocation } from "wouter";
 export default function ListadoProd({ madera }) {
 
   const [location] = useLocation();
-
   const filtro = location.split("/");
   const filtroSelec = filtro[filtro.length - 1];
 
-  let productosFiltrados;
+  const productosFiltrados = datos.productos.filter((p) => {
+    
+    const categoriaTodas = filtroSelec === "Catalogo" || filtroSelec === "Todas";
 
-  if (madera === "Todas" || madera == "" ) {
-    productosFiltrados = datos.productos.filter(p =>
-      filtroSelec === p.categoria || filtroSelec === "Catalogo"
-    );
-  } else {
-    productosFiltrados = datos.productos.filter(p =>
-      (filtroSelec === p.categoria || filtroSelec === "Catalogo") &&
-      madera === p.material 
-    );
-  }
+    const maderaTodas = madera === "" || madera === "Todas";
+
+    const coincideCategoria = categoriaTodas || filtroSelec === p.categoria;
+
+    const coincideMadera = maderaTodas || madera === p.material;
+
+    return coincideCategoria && coincideMadera;
+  });
 
   return (
     <div className="Listado">
+
       {productosFiltrados.map((producto) => (
         <Tarjeta
           key={producto.id}
           productos={producto}
         />
       ))}
+
     </div>
   );
 }
